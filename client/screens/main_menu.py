ba924aacc3c6248c2cf0700ccb026f5869d47c60
Daniel Boyd <daniel.boyd@portsolutionintegrators.com>
2024-09-30 12:12:25 -0400

    Fix SlideTransition issue

--- client/screens/main_menu.py
+++ client/screens/main_menu.py
@@ -5,4 +5,5 @@ Builder.load_file("client/layouts/main_menu.kv")
 
 class MainMenu(MDScreen):
     def on_play_button_click(self):
+        self.manager.transition.direction = "left"
         self.manager.current = 'select_player_count'
