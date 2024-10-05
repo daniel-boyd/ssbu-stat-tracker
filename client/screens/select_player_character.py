1ffd98c47524d8fc5cbc285a10c957bfa3316ec6
Daniel Boyd <daniel.boyd@portsolutionintegrators.com>
2024-10-05 12:06:12 -0400

    Recursion issues with reinit player_character_select

--- client/screens/select_player_character.py
+++ client/screens/select_player_character.py
@@ -5,6 +5,7 @@ from .. import shared
 
 class SelectPlayerCharacter(MDScreen):
     selected_player = None
+    is_pre_enter_called = False
 
     def __init__(self, kv_file=None, **kwargs):
         super().__init__(**kwargs)
@@ -12,19 +13,29 @@ class SelectPlayerCharacter(MDScreen):
             self.load_kv_file(kv_file)
 
     def on_pre_enter(self):
+        if self.is_pre_enter_called:
+            return
+        self.is_pre_enter_called = True
         kv_file = ""
-        if shared.get_current_match_value_from_key("num_players_string") == "3p":
-            kv_file = 'select_player_character_3p.kv'
+        if shared.get_current_match_value_from_key("num_players_str") == "3p":
+            kv_file = './layouts/select_player_character_3p.kv'
         else:
-            kv_file = 'select_player_character_2p.kv'
+            kv_file = './layouts/select_player_character_2p.kv'
         self.manager.reinitialize_screen(SelectPlayerCharacter, 'select_player_character', kv_file)
+
+    def on_enter(self):
+        self.is_pre_enter_called = False
 
     def load_kv_file(self, kv_file):
         """Load the specified .kv file."""
+        Builder.unload_file('./layouts/select_player_character_3p.kv')
+        Builder.unload_file('./layouts/select_player_character_2p.kv')
         Builder.load_file(kv_file)
 
     # Cleanup buttons and current_match keys on enter
     def cleanup(self):
+        Builder.unload_file('./layouts/select_player_character_3p.kv')
+        Builder.unload_file('./layouts/select_player_character_2p.kv')
         player_1_button = self.ids.player_1_button
         player_2_button = self.ids.player_2_button
         shared.set_current_match("player_1_character", "")
--- client/ssbu_screen_manager.py
+++ client/ssbu_screen_manager.py
@@ -1,11 +1,11 @@
 from kivymd.uix.screenmanager import ScreenManager
-from screens.select_player_character import SelectPlayerCharacter
+from .screens.select_player_character import SelectPlayerCharacter
 
 # Extends the ScreenManager to allow for reinitializing screens, loading varying .kv files when dynamic layouts are needed
 class SsbuScreenManager(ScreenManager):
     def reinitialize_screen(self, screen_class, screen_name, kv_file):
         """Reinitialize the specified screen by removing it and adding a new instance."""
-        new_screen = screen_class(kv_file=kv_file, name=screen_name)  # Pass the kv_file
+        new_screen = screen_class(kv_file=kv_file, name=screen_name)
         self.remove_widget(self.get_screen(screen_name))
         self.add_widget(new_screen)
         self.current = new_screen.name
