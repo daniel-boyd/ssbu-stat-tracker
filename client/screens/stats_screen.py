83f8743907e505e5ca662d9a2d3910a95da36b78
ColeBiggs <colebiggs29@gmail.com>
2024-10-08 22:30:53 -0400

    Added working deaths and clutches in

--- client/screens/stats_screen.py
+++ client/screens/stats_screen.py
@@ -14,7 +14,7 @@ class StatsScreen(MDScreen):
         player_2_stats.text = shared.get_current_match_value_from_key("players")[2]
         player_1_name = shared.get_current_match_value_from_key("players")[1]
         player_2_name = shared.get_current_match_value_from_key("players")[2]
-        # Set player names?
+            # Set player names?
         player_1_stats.txt = player_1_name
         player_2_stats.txt = player_2_name
 
@@ -23,5 +23,23 @@ class StatsScreen(MDScreen):
         player_2_stocks_taken = str(player_stocks.get(player_2_name,0))
         self.ids.player_1_stocks_taken.text = player_1_stocks_taken
         self.ids.player_2_stocks_taken.text = player_2_stocks_taken
+            # Player Deaths start
+        player_deaths = shared.get_current_match_value_from_key("player_deaths")
+        player_1_deaths = str(player_deaths.get(player_1_name,0))
+        player_2_deaths = str(player_deaths.get(player_2_name,0))
+        self.ids.player_1_deaths.text = player_1_deaths
+        self.ids.player_2_deaths.text = player_2_deaths
+            #Clutches 1v1 start
+        player_1v1_clutches = shared.get_current_match_value_from_key("player_1v1_clutches")
+        player_1_1v1_clutches = str(player_1v1_clutches.get(player_1_name,0))
+        player_2_1v1_clutches = str(player_1v1_clutches.get(player_2_name,0))
+        self.ids.player_1_1v1_clutches.text = player_1_1v1_clutches
+        self.ids.player_2_1v1_clutches.text = player_2_1v1_clutches
+            #Clutches 1v2 start
+        player_1v2_clutches = shared.get_current_match_value_from_key("player_1v2_clutches")
+        player_1_1v2_clutches = str(player_1v2_clutches.get(player_1_name,0))
+        player_2_1v2_clutches = str(player_1v2_clutches.get(player_2_name,0))
+        self.ids.player_1_1v2_clutches.text = player_1_1v1_clutches
+        self.ids.player_2_1v2_clutches.text = player_2_1v1_clutches
 
 pass
